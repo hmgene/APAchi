65e7e93e70d82c8c08029b2e50fb95aa035c6389
oannes <oannes@hm.ucdenver.edu>
2017-08-20 14:53:38 -0600

    install

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # APAchi
 Alternative Polyadenylation Tools for all different PolyA-seq data
+
+* read https://github.com/brentp/gobio/blob/master/bamregions/main.go  
+
--- web.go
+++ web.go
@@ -2,25 +2,34 @@ package main
 import(
 	"io"
 	"net/http"
-	"syscall"
-	"os"
+	//"syscall"
+	//"os"
 	"os/exec"
+	"log"
+	"fmt"
 )
+
+func get_bam(url string, region string) []byte {
+	//url := string("http://tanlab.ucdenver.edu/hmKim/Panc/Panc-1.bam");
+	//region := string("chr2:10000-200000");
+	out, err := exec.Command("samtools","view",url,region).Output();
+	if err != nil{ log.Fatal(err);}
+	return( out );
+}
 
 func hello(w http.ResponseWriter, r *http.Request){
 	io.WriteString(w,"hello!");
-	cmd, err0 := exec.LookPath("samtools");
-	if err0 != nil {
-		panic(err0);
-	}
-	err := syscall.Exec(cmd,[]string{"samtools","view","http://tanlab.ucdenver.edu/hmKim/Panc/Panc-1.bam","chr22:10000-200000"},os.Environ());
-	if err != nil {
-	}
+	//err := exec.LookPath("samtools");
+	//if err != nil { panic(err); }
 } 
 
 
 func main(){
 	mux := http.NewServeMux();
 	mux.HandleFunc("/",hello);
+	url := string("http://tanlab.ucdenver.edu/hmKim/Panc/Panc-1.bam");
+	region := string("chr2:10000-200000");
+	res :=get_bam(url,region);
+	fmt.Printf("%s\n",res);	
 	http.ListenAndServe(":8000",mux);
 }
